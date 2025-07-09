28fd824aa264143a359f4c1a7c7a48c4ad258edc
MohamedMamdouh4 <mohamedmamdouhgeeklabs@gmail.com>
2025-07-09 16:28:55 +0300

    final commit message

---
+++ test.js
@@ -0,0 +1,12 @@
+const x = async () => {
+  try {
+    const response = await fetch("https://api.example.com/data");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+};
